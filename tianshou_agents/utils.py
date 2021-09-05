89fa93cf735b5d142365349d45a7a413825d2ef0
Michal Gregor <michal@gregor.sk>
2021-09-05 11:17:26 +0200

    Added __dir__ to AgentLoggerWrapper.

--- tianshou_agents/utils.py
+++ tianshou_agents/utils.py
@@ -8,42 +8,42 @@ from torch.utils.tensorboard import SummaryWriter
 from typing import Any, List, Optional, Union, Tuple, Callable
 
 class VectorEnvRenderWrapper(gym.Wrapper):
-        def __init__(self, env: BaseVectorEnv):
-            super().__init__(env)
+    def __init__(self, env: BaseVectorEnv):
+        super().__init__(env)
 
-        def __len__(self) -> int:
-            """Return len(self), which is the number of environments."""
-            return self.env.env_num
+    def __len__(self) -> int:
+        """Return len(self), which is the number of environments."""
+        return self.env.env_num
 
-        def reset(self, id: Optional[Union[int, List[int], np.ndarray]] = None) -> np.ndarray:
-            return self.env.reset(id)
+    def reset(self, id: Optional[Union[int, List[int], np.ndarray]] = None) -> np.ndarray:
+        return self.env.reset(id)
 
-        def step(
-            self,
-            action: np.ndarray,
-            id: Optional[Union[int, List[int], np.ndarray]] = None
-        ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-            return self.env.step(action, id)
+    def step(
+        self,
+        action: np.ndarray,
+        id: Optional[Union[int, List[int], np.ndarray]] = None
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        return self.env.step(action, id)
 
-        def seed(
-            self, seed: Optional[Union[int, List[int]]] = None
-        ) -> List[Optional[List[int]]]:
-            return self.env.seed(seed)
+    def seed(
+        self, seed: Optional[Union[int, List[int]]] = None
+    ) -> List[Optional[List[int]]]:
+        return self.env.seed(seed)
 
-        def render(self, **kwargs: Any) -> List[Any]:
-            """Render all of the environments."""
-            self.env._assert_is_not_closed()
-            if self.env.is_async and len(self.env.waiting_id) > 0:
-                raise RuntimeError(
-                    f"Environments {self.env.waiting_id} are still stepping, cannot "
-                    "render them now.")
-            return self.env.workers[0].render(**kwargs)
+    def render(self, **kwargs: Any) -> List[Any]:
+        """Render all of the environments."""
+        self.env._assert_is_not_closed()
+        if self.env.is_async and len(self.env.waiting_id) > 0:
+            raise RuntimeError(
+                f"Environments {self.env.waiting_id} are still stepping, cannot "
+                "render them now.")
+        return self.env.workers[0].render(**kwargs)
 
-        def close(self) -> None:
-            return self.env.close()
+    def close(self) -> None:
+        return self.env.close()
 
-        def normalize_obs(self, obs: np.ndarray) -> np.ndarray:
-            return self.env.normalize_obs(obs)
+    def normalize_obs(self, obs: np.ndarray) -> np.ndarray:
+        return self.env.normalize_obs(obs)
 
 class AgentLoggerWrapper(BaseLogger):
     def __init__(self, agent: 'Agent', logger: BaseLogger):
@@ -52,6 +52,11 @@ class AgentLoggerWrapper(BaseLogger):
 
     def __getattr__(self, name):
         return getattr(self.preset, name)
+
+    def __dir__(self):
+        d = set(self.logger.__dir__())
+        d.update(set(super().__dir__()))
+        return d
 
     def write(self, step_type: str, step: int, data: LOG_DATA_TYPE) -> None:
         return self.logger.write(step_type, step, data)
