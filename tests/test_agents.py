898385ee83ac6211f3a7ec8029027210ce9eb777
Michal Gregor <michal@gregor.sk>
2022-08-16 13:16:52 +0200

    Added a Dict environment test to test_agents. * There is now a test that creates an SAC agent from preset and   trains it on a task with a Dict observation space. * Tuple spaces are not supported by BaseVectorEnv right now,   so no test for them.

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -3,12 +3,44 @@ import unittest
 from tianshou.env.venvs import DummyVectorEnv
 from tianshou_agents.methods.dqn import dqn_classic
 from tianshou_agents.methods.sac import sac_classic
+import numpy as np
+import gym
 
 # suppress tqdm output
 import os
 from functools import partialmethod
 from tqdm import tqdm
 tqdm.__init__ = partialmethod(tqdm.__init__, file=open(os.devnull, 'w'))
+
+class PendulumDict(gym.Wrapper):
+    def __init__(self):
+        env = gym.make('Pendulum-v1')
+        super().__init__(env)
+
+        self.observation_space = gym.spaces.Dict({
+            'part1': gym.spaces.Box(
+                    low=-1, high=1, shape=(2,), dtype=np.float32
+            ),
+            'part2': gym.spaces.Box(
+                low=-8, high=8, shape=(1,), dtype=np.float32
+            )    
+        })
+
+    def split_obs(self, obs):
+        return {'part1': obs[:2], 'part2': obs[-1:]}
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        return self.split_obs(obs), reward, done, info
+
+    def reset(self):
+        return self.split_obs(self.env.reset())
+
+gym.register(
+    id='PendulumDict-v0',
+    entry_point="test_agents:PendulumDict",
+    max_episode_steps=200
+)
 
 class TestAgentMixin:
     TaskName = ''
@@ -44,5 +76,9 @@ class TestAgentDQN(TestAgentMixin, unittest.TestCase):
 
 class TestAgentSAC(TestAgentMixin, unittest.TestCase):
     TaskName = 'MountainCarContinuous-v0'
+    AgentPreset = sac_classic
+    
+class TestAgentSACDictEnv(TestAgentMixin, unittest.TestCase):
+    TaskName = 'PendulumDict-v0'
     AgentPreset = sac_classic
     
